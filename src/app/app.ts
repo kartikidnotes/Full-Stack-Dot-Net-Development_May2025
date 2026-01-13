5e72bd3506528ebe0ed3a25ddd0196ffd0218573
kartikidnotes <kartiki.d@itpreneurpune.com>
2026-01-13 10:08:20 +0530

    angualar basic programs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Basic } from "./basic/basic";
+import { Datatypes } from "./datatypes/datatypes";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Basic, Datatypes],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angularprojects');
+
+  name="My name is IT Preneur!!!!!!!"
 }
---
+++ src/app/standaloneexample/standaloneexample.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-standaloneexample',
+  imports: [],
+  templateUrl: './standaloneexample.html',
+  styleUrl: './standaloneexample.css',
+})
+export class Standaloneexample {
+
+}
