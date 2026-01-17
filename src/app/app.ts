0f7016adcdd883ab705aa8de6a18e945d013362e
kartikidnotes <kartiki.d@itpreneurpune.com>
2026-01-17 12:16:06 +0530

    directicves

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Basic } from "./basic/basic";
 import { Datatypes } from "./datatypes/datatypes";
+import { Standaloneexample } from "./standaloneexample/standaloneexample";
+import { Lifecycle } from "./lifecycle/lifecycle";
+import { Structuraldirectives } from "./structuraldirectives/structuraldirectives";
+import { Attributedirective } from "./attributedirective/attributedirective";
+import { Componentdirective } from "./componentdirective/componentdirective";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Basic, Datatypes],
+  imports: [RouterOutlet, Basic, Datatypes, Standaloneexample, Lifecycle, Structuraldirectives, Attributedirective, Componentdirective],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
