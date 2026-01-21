0776b8fc9b91b4c45839a5d5376f746f65e6694f
Your kartikidnotes <kartiki.d@itpreneurpune.com>
2026-01-21 12:10:47 +0530

    updqated code

---
+++ src/app/app.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Basic } from "./basic/basic";
+import { Datatypes } from "./datatypes/datatypes";
+import { Standaloneexample } from "./standaloneexample/standaloneexample";
+import { Lifecycle } from "./lifecycle/lifecycle";
+import { Structuraldirectives } from "./structuraldirectives/structuraldirectives";
+import { Attributedirective } from "./attributedirective/attributedirective";
+import { Componentdirective } from "./componentdirective/componentdirective";
+import { Parentoutputdecorator } from './parentoutputdecorator/parentoutputdecorator';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Basic, Datatypes, Standaloneexample, Lifecycle, Structuraldirectives, Attributedirective, Componentdirective,Parentoutputdecorator],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angularprojects');
+
+  name="My name is IT Preneur!!!!!!!"
+}
